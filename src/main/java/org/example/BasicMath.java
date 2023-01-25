0203cbd94dafb0653dd45d6c3460c256302917b2
Joash Dellota <joash.dellota@novare.com.hk>
2023-01-25 15:07:30 +0800

    use given Basic math file with additional multiplication method

--- src/main/java/org/example/BasicMath.java
+++ src/main/java/org/example/BasicMath.java
@@ -1,17 +1,19 @@
 package org.example;
 
 public class BasicMath {
-    public double add(double input1, double input2){
-        return input1+input2;
+    public double add(double input1, double input2) {
+        return 0;
     }
-    public double subtract(double input1, double input2){
-        return input1-input2;
+    public double subtract(double input1, double input2) {
+        return 0;
     }
-    public double multiply(double input1, double input2){
-        return input1*input2;
+    public double multiply(double input1, double input2) {
+        return 0;
     }
-    public double divide(double input1, double input2){
-        return input1/input2;
+
+    public double divide(double input1, double input2) {
+        return 0;
     }
 }
+
 
